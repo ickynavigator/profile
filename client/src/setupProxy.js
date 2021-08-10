8a815cde455a7e591d934dc9995f788032ab625d
Obi Fortune <gabrielobi.of@gmail.com>
2021-08-10 21:49:53 +0100

    updated client and admin toml and fixed setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-module.exports = function (app: { use: (arg0: any) => void }) {
+module.exports = function (app) {
   app.use(
     createProxyMiddleware("/.netlify/functions", {
       target: "http://localhost:9000",
